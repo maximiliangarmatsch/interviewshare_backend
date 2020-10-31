344f7e93270570e3d62b3bbac9ce5d11275ac844
Maximilian Garmatsch <mail@maximiliangarmatsch.com>
2020-10-31 13:43:12 +0100

    cleanup

--- router.js
+++ router.js
@@ -7,18 +7,15 @@ const requireAuth = passport.authenticate('jwt', { session : false });
 const requireSignIn = passport.authenticate('local' ,{session: false});
 module.exports = function(app){
     //Protected  Routes through middleware
-        app.get('/',requireAuth, function(req, res ,next){
-            res.send({hi : 'there'})
-        })
-        
-
+    app.get('/',requireAuth, function(req, res ,next){
+        res.send({hi : 'there'})
+    })
+  
     //Authentication  Routes with Middleware
-        app.post('/signIn',requireSignIn, Authentication.signIn);
-        app.post('/eSignUp', Authentication.eSignUp);
-        app.get('./token', Authentication.token);
-        app.get('./logout',Authentication.logout);
+    app.post('/signIn',requireSignIn, Authentication.signIn);
+    app.post('/eSignUp', Authentication.eSignUp);
+    app.get('./token', Authentication.token);
+    app.get('./logout',Authentication.logout);
      //Utils Routes
-        app.get('/pdf',Pdf.create);
-        
-
+    app.get('/pdf',Pdf.create);
 }
