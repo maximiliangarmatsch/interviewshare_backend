501c8fefc5f9a2d86e787441659304ed3b02ab37
muhammadumerhack <umer1807f@aptechsite.net>
2020-11-06 16:47:07 +0500

    Heroku Fix#3

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,13 +7,13 @@ const thirdParty = require('./thirdParty/api')
 const pdf = require('./pdf/pdf')
 const csv = require('./csv/csv')
 
-module.exports = (function (app) {
+module.exports = function (app) {
   protectedRoutes(app)
   signIn(app)
   signUp(app)
   refreshToken(app)
   logout(app)
-  thirdParty(app)
-  pdf(app)
-  csv(app)
-})
+  // thirdParty(app)
+  // pdf(app)
+  // csv(app)
+}
