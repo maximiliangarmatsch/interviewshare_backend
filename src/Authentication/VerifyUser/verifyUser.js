cedd1b3bd6388dd291ce6a959ea12c73879d49e6
muhammadumerhack <umer1807f@aptechsite.net>
2021-02-05 02:22:37 +0500

    4-2-2021 Bug Free Code

--- src/Authentication/VerifyUser/verifyUser.js
+++ src/Authentication/VerifyUser/verifyUser.js
@@ -1,4 +1,5 @@
 const {getUserIdBySecret, setUserAsVerified} = require('../../Components/functions/auth/verifyUser')
+const {welcomeEmail} = require('../../Components/functions/email/emailer')
 exports.verifyUser = (req, res, next) => {
     const confirmationCode = req.params.confirmationCode
     getUserIdBySecret(confirmationCode,(err, id)=>{
@@ -6,11 +7,12 @@ exports.verifyUser = (req, res, next) => {
             return res.status(404).send({ status:"Err! Verification Failed",message: "Invalid Code passed or Code Expired" });
           }else{
             setUserAsVerified(id,(data)=>{
-                const verified = data.is_verified
-                if(verified == null){
+                if(data == null){
                     return res.status(401).send({ status:"Err! Already Verified",message: "User is already verified" });
                 }
-                if(verified){
+                const verified = data.is_verified   
+                if(verified == true){
+                    welcomeEmail(data.email)
                     return res.status(200).send({status:"Verified",message: "User Verified Successfully"})
                 }
             })
--- src/Models/mutations/verifyUser.js
+++ src/Models/mutations/verifyUser.js
@@ -1,9 +1,10 @@
 module.exports =(`
 mutation ($id: uuid!) {
-    verified:update_isa_User(where: {id: {_eq: $id},, is_verified: {_eq: false}}, _set: {is_verified: true}){
-      returning{
-        is_verified
-      }
-    }  
+  verified:update_isa_User(_set: {is_verified: true}, where: {id: {_eq: $id}, is_verified: {_eq: false}}) {
+    returning {
+      is_verified,
+      email
+    }
+  } 
 }
 `)
